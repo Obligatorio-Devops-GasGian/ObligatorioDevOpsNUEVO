577d4d624479ab803470e6be5c0287cf7b9df198
Gastón <gasvaryt@gmail.com>
2025-06-28 20:15:14 -0300

    Intento reparacion seed-data 12

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+VOTE_URL=${VOTE_URL:-http://44.222.184.226/}
 
-# create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" \
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" \
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" \
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
