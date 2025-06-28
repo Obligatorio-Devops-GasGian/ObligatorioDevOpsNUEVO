5ea56719cc9f068a15f1ee5cedd371f40ead2bf9
Gastón <gasvaryt@gmail.com>
2025-06-28 20:13:53 -0300

    Intento reparacion seed-data 12

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,8 +1,8 @@
-VOTE_URL=${VOTE_URL:-http://44.211.55.160/}
+VOTE_URL=${VOTE_URL:-http://44.222.184.226/}
 
 ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" \
-   -H "Host: 44.211.55.160"  "$VOTE_URL"
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
 ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" \
-   -H "Host: 44.211.55.160"  "$VOTE_URL"
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
 ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" \
-   -H "Host: 44.211.55.160"  "$VOTE_URL"
+   -H "Host: 44.222.184.226"  "$VOTE_URL"
